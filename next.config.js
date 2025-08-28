e1db27a12f331f48cc15e6d851240277332ca5aa
Praveen Kumar <122990319+Praveenkumar76@users.noreply.github.com>
2025-08-28 10:16:23 +0530

    netlify

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
-        serverActions: {},
-    },
+    output: 'export',
+    trailingSlash: true,
     images: {
+        unoptimized: true,
         domains: ["zjpubvbqfagluanwtbjx.supabase.co"],
     },
     async headers() {
